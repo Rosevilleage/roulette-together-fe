77e7d0cc1bdf690995a340259a121fe81078edc7
rosevi <chanhee.frontend@gmail.com>
2026-02-25 18:20:18 +0900

    🐛 fix: open graph 이미지 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,7 @@ export const metadata: Metadata = {
     siteName: siteConfig.name,
     images: [
       {
-        url: '/og-image.png',
+        url: '/og-img.png',
         width: 1200,
         height: 630,
         alt: siteConfig.title
@@ -55,7 +55,7 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     title: siteConfig.title,
     description: siteConfig.description,
-    images: ['/og-image.png']
+    images: ['/og-img.png']
   },
   icons: {
     icon: [
